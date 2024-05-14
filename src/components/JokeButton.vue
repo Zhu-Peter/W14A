01e7f47efae6e09b113e58b2a01a23c4dea2d05b
Zhu-Peter <gzhu@ualberta.ca>
2024-05-13 19:13:41 -0600

    added ability to get more than one joke

--- src/components/JokeButton.vue
+++ src/components/JokeButton.vue
@@ -10,16 +10,14 @@ export default {
     name: 'JokeButton',
     data() {
         return {
-            isJoke: false,
         }
     },
     methods: {
         getJoke: function () {
-            if(!this.isJoke){
                 axios.request({
                     url: 'https://ron-swanson-quotes.herokuapp.com/v2/quotes'
                 }).then((response)=>{this.$root.$emit('jokeListener', response.data[0]); this.isJoke = true}).catch((error)=>{console.log(error)})
-            }
+            
         }
     },
 }
